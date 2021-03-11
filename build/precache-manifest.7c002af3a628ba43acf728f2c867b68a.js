a2538020da722af16808f576ca122f47e2e63086
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-11 17:34:22 +0200

    Deploy

---
+++ build/precache-manifest.7c002af3a628ba43acf728f2c867b68a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e2e19b112c99ee4bcfd7ed3c466d4c",
+    "url": "/TBH2/index.html"
+  },
+  {
+    "revision": "834a45dfca794d62b2b4",
+    "url": "/TBH2/static/css/main.e265ec43.chunk.css"
+  },
+  {
+    "revision": "2d947974f178beed2384",
+    "url": "/TBH2/static/js/2.20df1dd5.chunk.js"
+  },
+  {
+    "revision": "834a45dfca794d62b2b4",
+    "url": "/TBH2/static/js/main.c05b6851.chunk.js"
+  },
+  {
+    "revision": "9511b02935d6ede464f3",
+    "url": "/TBH2/static/js/runtime-main.fd073335.js"
+  }
+]);
